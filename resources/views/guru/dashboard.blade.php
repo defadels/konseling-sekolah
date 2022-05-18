9d53e009904524e0bb924d42aa321cee38573ab1
Muhammad Fadhil Adha <fadhil.sonic@gmail.com>
2022-05-18 13:38:56 +0700

    Much changes

---
+++ resources/views/guru/dashboard.blade.php
@@ -0,0 +1,1 @@
+@extends('layout.guru_layout')
--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,114 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+
+Route::prefix('master')->name('master.')->namespace('Master')->group(function(){
+    Route::get('/', 'DashboardController@index')->name('dashboard');
+
+    Route::get('profile/{profile}','ProfileController@show')->name('profile');
+
+    Route::get('laporan','LaporanController@index')->name('laporan');
+
+    Route::get('siswa','SiswaController@index')->name('siswa');
+    Route::get('siswa/create','SiswaController@create')->name('siswa.create');
+    Route::post('siswa','SiswaController@store')->name('siswa.store');
+    Route::get('siswa/{siswa}','SiswaController@edit')->name('siswa.edit');
+    Route::put('siswa/{siswa}','SiswaController@update')->name('siswa.update');
+    Route::get('siswa/lihat/{siswa}','SiswaController@show')->name('siswa.show');
+    Route::delete('siswa/{siswa}','SiswaController@destroy')->name('siswa.destroy');
+
+    Route::get('guru','GuruController@index')->name('guru');
+    Route::get('guru/create','GuruController@create')->name('guru.create');
+    Route::post('guru','GuruController@store')->name('guru.store');
+    Route::get('guru/{guru}','GuruController@edit')->name('guru.edit');
+    Route::put('guru/{guru}','GuruController@update')->name('guru.update');
+    Route::get('guru/lihat/{guru}','GuruController@show')->name('guru.show');
+    Route::delete('guru/{guru}','GuruController@destroy')->name('guru.destroy');
+
+    Route::get('kelas','KelasController@index')->name('kelas');
+    Route::get('kelas/create','KelasController@create')->name('kelas.create');
+    Route::post('kelas','KelasController@store')->name('kelas.store');
+    Route::get('kelas/{kelas}','KelasController@edit')->name('kelas.edit');
+    Route::put('kelas/{kelas}','KelasController@update')->name('kelas.update');
+    Route::get('kelas/lihat/{kelas}', 'KelasController@show')->name('kelas.show');
+    Route::delete('kelas/{kelas}','KelasController@destroy')->name('kelas.destroy');
+
+    Route::get('bimbingan-konseling/masuk','BKController@index')->name('bimbingan.masuk');
+    Route::get('bimbingan-konseling/masuk/lihat/{bk}','BKController@show')->name('bimbingan.masuk.show');
+    Route::get('bimbingan-konseling/masuk/tanggapi/{bk}','BKController@edit')->name('bimbingan.masuk.tanggapi');
+    Route::put('bimbingan-konseling/masuk/{bk}','BKController@update')->name('bimbingan.masuk.update');
+
+    Route::get('bimbingan-konseling/ditanggapi','BKTanggapanController@index')->name('bimbingan.ditanggapi');
+    Route::get('bimbingan-konseling/ditanggapi/lihat/{bk}', 'BKTanggapanController@show')->name('bimbingan.ditanggapi.show');
+});
+
+Route::prefix('guru')->name('guru.')->middleware('auth','tolakselainguru')->namespace('Guru')->group(function(){
+    Route::get('/', 'DashboardController@index')->name('dashboard');
+
+    Route::get('profile/{profile}','ProfileController@show')->name('profile');
+
+    Route::get('laporan','LaporanController@index')->name('laporan');
+
+    Route::get('siswa','SiswaController@index')->name('siswa');
+    Route::get('siswa/create','SiswaController@create')->name('siswa.create');
+    Route::post('siswa','SiswaController@store')->name('siswa.store');
+    Route::get('siswa/{siswa}','SiswaController@edit')->name('siswa.edit');
+    Route::put('siswa/{siswa}','SiswaController@update')->name('siswa.update');
+    Route::get('siswa/lihat/{siswa}','SiswaController@show')->name('siswa.show');
+    Route::delete('siswa/{siswa}','SiswaController@destroy')->name('siswa.destroy');
+
+    Route::get('guru','GuruController@index')->name('guru');
+    Route::get('guru/create','GuruController@create')->name('guru.create');
+    Route::post('guru','GuruController@store')->name('guru.store');
+    Route::get('guru/{guru}','GuruController@edit')->name('guru.edit');
+    Route::put('guru/{guru}','GuruController@update')->name('guru.update');
+    Route::get('guru/lihat/{guru}','GuruController@show')->name('guru.show');
+    Route::delete('guru/{guru}','GuruController@destroy')->name('guru.destroy');
+
+    Route::get('kelas','KelasController@index')->name('kelas');
+    Route::get('kelas/create','KelasController@create')->name('kelas.create');
+    Route::post('kelas','KelasController@store')->name('kelas.store');
+    Route::get('kelas/{kelas}','KelasController@edit')->name('kelas.edit');
+    Route::put('kelas/{kelas}','KelasController@update')->name('kelas.update');
+    Route::get('kelas/lihat/{kelas}', 'KelasController@show')->name('kelas.show');
+    Route::delete('kelas/{kelas}','KelasController@destroy')->name('kelas.destroy');
+
+    Route::get('bimbingan-konseling/masuk','BKController@index')->name('bimbingan.masuk');
+    Route::get('bimbingan-konseling/masuk/lihat/{bk}','BKController@show')->name('bimbingan.masuk.show');
+    Route::get('bimbingan-konseling/masuk/tanggapi/{bk}','BKController@edit')->name('bimbingan.masuk.tanggapi');
+    Route::put('bimbingan-konseling/masuk/{bk}','BKController@update')->name('bimbingan.masuk.update');
+
+    Route::get('bimbingan-konseling/ditanggapi','BKTanggapanController@index')->name('bimbingan.ditanggapi');
+    Route::get('bimbingan-konseling/ditanggapi/lihat/{bk}', 'BKTanggapanController@show')->name('bimbingan.ditanggapi.show');
+});
+
+Route::prefix('siswa')->name('siswa.')->middleware('auth','tolakselainsiswa')->namespace('Siswa')->group(function(){
+    Route::get('/','DashboardController@index')->name('dashboard');
+
+    Route::get('profile/{profile}','ProfileController@show')->name('profile');
+
+    Route::get('bimbingan-konseling/karir','BKKarirController@index')->name('bimbingan.karir');
+    Route::get('bimbingan-konseling/karir/create', 'BKKarirController@create')->name('bimbingan.karir.create');
+    Route::post('bimbingan-konseling/karir', 'BKKarirController@store')->name('bimbingan.karir.store');
+    Route::get('bimbingan-konseling/karir/{data_bk}/show', 'BKKarirController@show')->name('bimbingan.karir.show');
+
+    Route::get('bimbingan-konseling/konseling-kelompok','BKKonselingKelompokController@index')->name('bimbingan.konseling.kelompok');
+    Route::get('bimbingan-konseling/konseling-kelompok/create', 'BKKonselingKelompokController@create')->name('bimbingan.konseling.kelompok.create');
+    Route::post('bimbingan-konseling/konseling-kelompok', 'BKKonselingKelompokController@store')->name('bimbingan.konseling.kelompok.store');
+    Route::get('bimbingan-konseling/konseling-kelompok/{data_bk}/show', 'BKKonselingKelompokController@show')->name('bimbingan.konseling.kelompok.show');
+    
+    Route::get('bimbingan-konseling/kelompok','BKKelompokController@index')->name('bimbingan.kelompok');
+    Route::get('bimbingan-konseling/kelompok/create', 'BKKelompokController@create')->name('bimbingan.kelompok.create');
+    Route::post('bimbingan-konseling/kelompok', 'BKKelompokController@store')->name('bimbingan.kelompok.store');
+    Route::get('bimbingan-konseling/kelompok/{data_bk}/show', 'BKKelompokController@show')->name('bimbingan.kelompok.show');
+
+    Route::get('bimbingan-konseling/pribadi','BKPribadiController@index')->name('bimbingan.pribadi');
+    Route::get('bimbingan-konseling/pribadi/create', 'BKPribadiController@create')->name('bimbingan.pribadi.create');
+    Route::post('bimbingan-konseling/pribadi', 'BKPribadiController@store')->name('bimbingan.pribadi.store');
+    Route::get('bimbingan-konseling/pribadi/{data_bk}/show', 'BKPribadiController@show')->name('bimbingan.pribadi.show');
+});
+
+Auth::routes(['verify' => true]);
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
