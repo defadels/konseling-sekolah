b2f0c734b199460771f978f109a09301f18ef6a5
Muhammad Fadhil Adha <fadhil.sonic@gmail.com>
2022-05-30 22:35:30 +0700

    WOW

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -73,7 +73,7 @@ class RegisterController extends Controller
     protected function create(array $data)
     {
         return User::create([
-            'nip' => $data['nis'],
+            'nip' => $data['nip'],
             'nama' => $data['nama'],
             'email' => $data['email'],
             'tempat_lahir' => $data['tempat_lahir'],
